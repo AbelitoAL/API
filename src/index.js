2f44feec00876719368bfe1b5e8d3cd00e555fc7
mariobros123413 <j.mario18@hotmail.es>
2023-06-11 01:22:20 -0400

    rmant

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import morgan from "morgan"
 import rutas from "./routes/persona.routes.js"
 import activo from './routes/activo.routes.js'
 import ubi from './routes/ubicacion.routes.js'
-
+import mant from './routes/mantenimiento.routes.js'
 const app = express()
 
 app.use(morgan('dev'));
@@ -16,5 +16,5 @@ app.use(express.urlencoded({extended: false}))
 app.use(rutas)
 app.use(activo)
 app.use(ubi)
-
+app.use(mant)
 app.listen(process.env.PORT||5000)
