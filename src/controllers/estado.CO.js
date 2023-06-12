2c2ec0c6f43a58f6517d1c1028288ed17c1c4446
mariobros123413 <j.mario18@hotmail.es>
2023-06-11 22:37:13 -0400

    estadoa

--- src/controllers/estado.CO.js
+++ src/controllers/estado.CO.js
@@ -3,7 +3,7 @@ import {consul} from "../db.js"
 export const createEstado = async (req, res) => {
     try {
         const { nombre} = req.body
-        consul.query('INSERT INTO estado (nombre) VALUES ($1,$2)', [nombre])
+        consul.query('INSERT INTO estado (nombre) VALUES ($1)', [nombre])
         res.send('estado creado')
     } catch (error) {
         res.send("ERROR CREATE ESTADO")
