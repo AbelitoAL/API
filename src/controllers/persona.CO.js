808d46646ed9fef14c966fce5300c5ddc3baaaa5
fabio <fabioarredondo44@gmail.com>
2023-10-30 00:25:44 -0400

    Abelito puto

--- src/controllers/persona.CO.js
+++ src/controllers/persona.CO.js
@@ -184,7 +184,7 @@ export const createuser = async (req, res) => {
         const rol = 1
         const intentos = 0
         await consul.query('INSERT INTO persona (ci, nombre, email) VALUES ($1,$2,$3)', [ci, nombre, email])
-        await consul.query('INSERT INTO administrador (cipersona,usuario,contrasena,rol_id) VALUES ($1,$2,$3,$4,$)', [ci, UsuI, Npass,rol])
+        await consul.query('INSERT INTO administrador (cipersona,usuario,contrasena,rol_id) VALUES ($1,$2,$3,$4)', [ci, UsuI, Npass,rol])
         res.send('usuario creado')
     } catch (error) {
         res.send("ERROR")
